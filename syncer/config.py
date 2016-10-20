b8c821687db91287e26fad6df33fce70e9cf9e5c
aisensiy <aisensiy@163.com>
2016-10-20 17:48:51 +0800

    update default env

--- syncer/config.py
+++ syncer/config.py
@@ -1,7 +1,7 @@
 import os
 
 LAST_EVENT_FILE = os.getenv('STORAGE', '/data/last_event')
-CDE_EVENT_ENTRYPOINT = os.getenv('CDE_EVENT_ENTRYPOINT', 'http://controller.aisensiy.com/stack_events')
-KETSU_ENTRYPOINT = os.getenv('KETSU_ENTRYPOINT', 'http://ketsu-backend.aisensiy.com')
+CDE_EVENT_ENTRYPOINT = os.getenv('CDE_EVENT_ENTRYPOINT', 'http://controller.tw-cde.com/stack_events')
+KETSU_ENTRYPOINT = os.getenv('KETSU_ENTRYPOINT', 'http://ketsu-api.tw-cde.com')
 KETSU_USERNAME = os.getenv('KETSU_USERNAME', 'admin')
 KETSU_PASSWORD = os.getenv('KETSU_PASSWORD', '123')
